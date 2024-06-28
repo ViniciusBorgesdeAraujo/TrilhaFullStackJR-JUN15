da0693cf08096af98443bb023f5cfccfedb802eb
Vinicius Borges <105869015+ViniciusBorgesdeAraujo@users.noreply.github.com>
2024-06-28 15:17:45 -0300

    backend

---
+++ data/products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    name: "HTML5",
+    imageUrl: "https://logowik.com/content/uploads/images/492_html5.jpg",
+    description:
+      "HTML (HyperText Markup Language) é a linguagem de marcação padrão para criar páginas e aplicativos da web. Ele fornece a estrutura de uma página da web, permitindo a exibição de texto, imagens e outros multimídia em um navegador.",
+    price: 39,
+    countInStock: 100,
+  },
+  {
+    name: "CSS3",
+    imageUrl: "https://logowik.com/content/uploads/images/123_css3.jpg",
+    description:
+      "CSS (Cascading Style Sheets) é uma linguagem de folha de estilo usada para descrever a apresentação de um documento escrito em HTML ou XML. O CSS descreve como os elementos devem ser renderizados na tela, no papel ou em outras mídias.",
+    price: 50,
+    countInStock: 100,
+  },
+  {
+    name: "JavaScript",
+    imageUrl: "https://logowik.com/content/uploads/images/3799-javascript.jpg",
+    description:
+      "JavaScript é uma linguagem de programação versátil e de alto nível que é uma das principais tecnologias da World Wide Web, junto com HTML e CSS. É amplamente usada para criar efeitos interativos dentro dos navegadores da web.",
+    price: 80,
+    countInStock: 100,
+  },
+  {
+    name: "ReactJs",
+    imageUrl: "https://logowik.com/content/uploads/images/react.jpg",
+    description:
+      "ReactJs é uma biblioteca JavaScript para construção de interfaces de usuário. Desenvolvida pelo Facebook, permite aos desenvolvedores criar grandes aplicativos web que podem atualizar e renderizar de forma eficiente em resposta às mudanças de dados.",
+    price: 100,
+    countInStock: 100,
+  },
+  {
+    name: "NodeJs",
+    imageUrl: "https://logowik.com/content/uploads/images/nodejs.jpg",
+    description:
+      "NodeJs é um ambiente de execução JavaScript de código aberto, multiplataforma, back-end, que é executado no motor V8 e executa código JavaScript fora de um navegador web. É usado para construir aplicações de rede escaláveis.",
+    price: 110,
+    countInStock: 100,
+  },
+  {
+    name: "MongoDB",
+    imageUrl:
+      "https://logowik.com/content/uploads/images/mongodb9740.logowik.com.webp",
+    description:
+      "MongoDB é um programa de banco de dados orientado a documentos, multiplataforma e de código aberto. Classificado como um programa de banco de dados NoSQL, o MongoDB usa documentos JSON-like com esquemas opcionais.",
+    price: 38,
+    countInStock: 100,
+  },
+  {
+    name: "VsCode",
+    imageUrl:
+      "https://logowik.com/content/uploads/images/visual-studio-code7642.jpg",
+    description:
+      "Visual Studio Code (VsCode) é um editor de código-fonte gratuito criado pela Microsoft para Windows, Linux e macOS. Recursos incluem suporte para depuração, realce de sintaxe, conclusão inteligente de código, snippets e refatoração de código.",
+    price: 10,
+    countInStock: 100,
+  },
+  {
+    name: "Git",
+    imageUrl: "https://logowik.com/content/uploads/images/git6963.jpg",
+    description:
+      "Git é um sistema de controle de versão distribuído para rastrear mudanças no código-fonte durante o desenvolvimento de software. É projetado para coordenar o trabalho entre programadores, mas pode ser usado para rastrear mudanças em qualquer conjunto de arquivos.",
+    price: 20,
+    countInStock: 100,
+  },
+];
+
+module.exports = products;
