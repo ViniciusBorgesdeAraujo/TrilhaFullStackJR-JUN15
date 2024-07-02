4d829665663cc80577362c9153f604f2375d499f
Vinicius Borges <105869015+ViniciusBorgesdeAraujo@users.noreply.github.com>
2024-07-02 14:27:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
 
 connectDB();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(express.json());
 
